8e815e57f590cc1b97e5baee384500b7495d9c60
Tavinho526 <86014076+Tavinho526@users.noreply.github.com>
2024-01-04 19:39:06 -0300

    Create desafio2.js

---
+++ desafio2.js
@@ -0,0 +1,25 @@
+function tot(saldoVitorias, saldoDerrotas){
+  let saldoDeRanqueadas = saldoVitorias - saldoDerrotas
+  return saldoDeRanqueadas
+}
+
+let saldoVitorias = tot(100, 50)
+let nivel = ""
+
+if(saldoVitorias <= 10){
+  nivel = "Ferro"
+}else if(saldoVitorias >= 11 && saldoVitorias <= 20){
+  nivel = "Broze"
+}else if(saldoVitorias >= 21 && saldoVitorias <= 50){
+  nivel = "Prata"
+}else if(saldoVitorias >= 51 && saldoVitorias <= 80){
+  nivel = "Ouro"
+}else if(saldoVitorias >= 81 && saldoVitorias <=90){
+  nivel = "Diamante"
+}else if(saldoVitorias >= 91 && saldoVitorias <= 100){
+  nivel = "Lendário"
+}else{
+  nivel = "Imortal"
+}
+
+console.log("O Herói tem de saldo " + saldoVitorias + " e está no nível de " + nivel)
